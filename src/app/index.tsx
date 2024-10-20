f94c792a774309df9da30977c9b1ff1a7473f8b3
Kohei-Takano <a8121153@gsuite.si.aoyama.ac.jp>
2024-10-20 14:24:27 +0900

    index.tsxを編集

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,33 @@
-import {View,Text}from'react-native'
+import {View,Text,StyleSheet}from'react-native'
 
 const Index=():JSX.Element=>{
     return(
-        <View>
-            <Text>Hello World</Text>
+        <View style={styles.container}>
+            <View>
+                <View>
+                    <Text>購入した食品</Text>
+                </View>
+                <View>
+                    <Text>ガリガリ君</Text>
+                    <Text>1個</Text>
+                    <Text>130円</Text>
+                </View>
+                <View>
+                    <Text>セブンイレブン</Text>
+                    <Text>2024年10月20日10時34分</Text>
+                </View>
+            </View>
+            <View>
+                <Text>＋</Text>
+            </View>
         </View>
     )
 }
+const styles=StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center'
+    }
+})
 export default Index
